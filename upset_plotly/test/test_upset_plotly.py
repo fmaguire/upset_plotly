dce5b4d3eaeb44ddbaeff7da4d6fd49e3c7d3b48
Finlay Maguire <finlaymaguire@gmail.com>
2020-12-22 23:36:03 -0400

    Update demo

--- upset_plotly/test/test_upset_plotly.py
+++ upset_plotly/test/test_upset_plotly.py
@@ -4,8 +4,13 @@ import upsetplot
 from upset_plotly import plot
 
 def test_plot():
-    output_path = Path('plot.html')
+    html_output_path = Path('plot.html')
+    png_output_path = Path('plot.png')
     df = upsetplot.generate_counts()
     fig = plot.upset_plotly(df, 'test')
-    fig.write_html(str(output_path))
-    assert output_path.exists()
+    fig.write_html(str(html_output_path))
+    fig.write_image(str(png_output_path))
+    assert html_output_path.exists()
+    assert png_output_path.exists()
+
+
